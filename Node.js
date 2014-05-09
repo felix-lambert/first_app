f9c391ad67c750427ef271c77cadd76d76113999
felix <lambertfelix8@gmail.com>
2014-05-09 13:25:47 +0200

    hello world

--- Node.js
+++ Node.js
@@ -1,6 +1,15 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Salut tout le monde !');
+// web.js
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
 });
-server.listen(8080);
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
