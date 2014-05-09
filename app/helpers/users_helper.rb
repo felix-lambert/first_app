b075b20574c604c589d7851291cfe9a65a2788e8
felix <lambertfelix8@gmail.com>
2014-05-09 12:11:47 +0200

    démarrage

--- app/helpers/users_helper.rb
+++
@@ -1,7 +0,0 @@
-module UsersHelper
-  def gravatar_for(user, options = { :size => 50 })
-    gravatar_image_tag(user.email.downcase, :alt => user.nom,
-                                            :class => 'gravatar',
-                                            :gravatar => options)
-  end
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Myapp::Application.config.secret_token = 'b312e3682199570a6faccfb771922b72da445f47ffcb29a49e0d0aeb169f1ebd530d85b10f703b65eef1dd705f6e902d8106621a9322f0f2e0435febc057478e'
